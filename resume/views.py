a477ebfa2dd6fc95d8b4477ed660766f265af1df
Ankit Kumar Singh <ankitkumarsingh9688@gmail.com>
2024-08-30 18:20:33 +0530

    t w 2

--- resume/views.py
+++ resume/views.py
@@ -1,5 +1,67 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 # Create your views here.
 def home(request):
     return render (request,"home.html")
+
+def about(request):
+    return render (request,"about.html")
+
+def projects(request):
+    projects_show=[
+        {
+            "title" : "Sporting_Edge_Academy",
+            "path": "images/sportsadmy.png"
+
+        },
+        {
+            "title" : "Portfolio",
+            "path": "images/portfolio.png"
+
+        },
+        {
+            "title" : "College_Portal",
+            "path": "images/collegeportal.png"
+
+        },
+        {
+            "title" : "Internship_Hub",
+            "path": "images/internhub.png"
+
+        },
+
+    ]
+    return render (request,"projects.html",{"projects_show":projects_show})
+
+
+def experience(request):
+    experience=[
+        {
+            "company":  "Precursor Info Solution Pvt. Ltd. , Lucknow ",
+            "position": "Intern as Python Developer"
+             
+        }
+       
+    ]
+    
+    return render (request,"experience.html",{"experience":experience})
+
+def certificate(request):
+    return render (request, "certificate.html")
+
+def contact(request):
+    return render (request, "contact.html")
+
+def resume(request):
+    resume_path="myapp/resume.pdf"
+    resume_path=staticfiles_storage.path(resume_path)
+    if staticfiles_storage.exists(resume_path):
+        with open (resume_path,"rb")as resume_files:
+            response=HttpResponse(resume_files.read(),content_type="application/pdf")
+            response['Content-Disposition']='attachment'; filename="resume.pdf"
+            return response
+    else:
+        return HttpResponse("resume not found", status=404)     
+
